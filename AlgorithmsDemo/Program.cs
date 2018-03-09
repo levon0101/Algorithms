189816253f6225a89e8e6be164250631d5937804
levon0101 <levon.ecs@gmail.com>
2018-03-09 15:14:25 +0400

    First Commite

---
+++ AlgorithmsDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BinarySearch;
+
+namespace AlgorithmsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int[] MyArray = new int[1000];
+
+            for (int i = 0; i < MyArray.Length; i++)
+            {
+                MyArray[i] = r.Next(10000); 
+            }
+
+            Array.Sort(MyArray);
+
+            for (int i = 0; i < MyArray.Length; i++)
+            {
+                Console.WriteLine($"index i = {i} value = {MyArray[i]}");
+            }
+            
+            BinarySearcher.Search(MyArray,0);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BinarySearch/BinarySearcher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    public class BinarySearcher
+    {
+        public static int Search(int[] myArray,int toFind)
+        {
+            int index = myArray.Length / 2;
+
+            return 0;
+        }
+    }
+}
