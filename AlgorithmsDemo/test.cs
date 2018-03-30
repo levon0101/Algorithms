5dc60af798b5a71ff915e6e6f5c93eb75a9933f0
levon0101 <levon.ecs@gmail.com>
2018-03-31 02:04:20 +0400

    test

---
+++ AlgorithmsDemo/test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsDemo
+{
+    class test
+    {
+
+        public int[] arr;
+        public test(int count)
+        {
+            arr = new int[count];
+        }
+
+        public int VarCounter(string s)
+        {
+            foreach (char c in s)
+            {
+
+            }
+
+            return 0;
+        }
+    }
+}
